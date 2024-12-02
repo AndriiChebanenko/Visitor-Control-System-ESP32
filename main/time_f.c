d76c9ba1b6eaa9a03639c300e7592d1fba4d2304
Andrii Chebanenko <andriy040105@gmail.com>
2024-12-02 14:46:41 +0200

    Add get_current_datetime function

--- main/time_f.c
+++ main/time_f.c
@@ -1,3 +1,4 @@
+#include "freertos/projdefs.h"
 #include <time.h>
 #include <stdio.h>
 #include <esp_log.h>
@@ -6,21 +7,21 @@
 static const char *TAG = "Time";
 
 void initialize_sntp(void) {
+	ESP_LOGI(TAG, "Initializing SNTP...");
     esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
     esp_sntp_setservername(0, "pool.ntp.org");
     esp_sntp_init();
 
     time_t now = 0;
     struct tm timeinfo = { 0 };
-    int retry = 5;
-    const int retry_delay = 2000;
-    while (timeinfo.tm_year < (2023 - 1900) && retry > 0) {
-        ESP_LOGI(TAG, "Waiting for time to be set... (%d retries left)", retry);
-        vTaskDelay(pdMS_TO_TICKS(retry_delay));
-        time(&now);
-        localtime_r(&now, &timeinfo);
-        retry--;
+    int retry = 0;
+    const int retry_count = 5;
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
+    time(&now);
+    localtime_r(&now, &timeinfo);
 
     if (timeinfo.tm_year < (2023 - 1900)) {
         ESP_LOGE(TAG, "Failed to synchronize time");
@@ -30,21 +31,33 @@ void initialize_sntp(void) {
 }
 
 char* get_current_date(void) {
-    static char date[11];
+    static char date_str[36];
     time_t now;
     struct tm timeinfo;
     time(&now);
     localtime_r(&now, &timeinfo);
-    snprintf(date, sizeof(date), "%02d.%02d.%04d", timeinfo.tm_mday, timeinfo.tm_mon + 1, timeinfo.tm_year + 1900);
-    return date;
+    snprintf(date_str, sizeof(date_str), "%02d/%02d/%04d", timeinfo.tm_mday, timeinfo.tm_mon + 1, timeinfo.tm_year + 1900);
+    return date_str;
 }
 
 char* get_current_time(void) {
-    static char time_str[9];
+    static char time_str[11];
     time_t now;
     struct tm timeinfo;
     time(&now);
     localtime_r(&now, &timeinfo);
     snprintf(time_str, sizeof(time_str), "%02d:%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
     return time_str;
+}
+
+char* get_current_datetime(void) {
+	static char datetime_str[48];
+    time_t now;
+    struct tm timeinfo;
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    sprintf(datetime_str, "%02d:%02d:%02d %02d/%02d/%04d",
+    						timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec,
+    						timeinfo.tm_mday, timeinfo.tm_mon + 1, timeinfo.tm_year + 1900);
+    return datetime_str;
 }
